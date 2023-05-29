e84e5e892bf82187a5a302f37821afbf53a7e2d3
Amjad <51831261+followamjad@users.noreply.github.com>
2023-05-29 08:40:05 +0700

    home

--- src/comp/Home.js
+++ src/comp/Home.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+import BannerImage from "../img/newne.gif";
+import Navbar from "./Navbar";
+import { FiArrowRight } from "react-icons/fi";
 
 const Home = () => {
   return (
-    <div className='home-container'>
-        <Navbar/>
-    </div>
-  )
-}
+    <div className="home-container">
+      <Navbar />
+      <div className="home-banner-container">
+        <div className="home-image-section">
+          <img src={BannerImage} alt="" />
+        </div>
+        <p className="primary-text">
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          Walk With Us To The Moon
+        </p>
 
-export default Home
+      </div>
+    </div>
+  );
+};
+
+export default Home;
