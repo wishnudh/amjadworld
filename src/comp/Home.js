18c6d1805da763bfeda038f73b4707e553db2c68
wishnudh <47201896+wishnudh@users.noreply.github.com>
2023-06-03 14:21:36 +0700

    Update Image Center & Toggle Image
    Using framer motion

--- src/comp/Home.js
+++ src/comp/Home.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from 'react';
 import BannerImage from "../img/newne.gif";
 import Navbar from "./Navbar";
 import { FiArrowRight } from "react-icons/fi";
+import { motion } from 'framer-motion';
+import AnotherImage from "../img/flip.gif";
+
+
+
 
 const Home = () => {
+  const [imageSrc, setImageSrc] = useState(BannerImage);
+
+  const handleClick = () => {
+    setImageSrc((prevImageSrc) =>
+      prevImageSrc === BannerImage ? AnotherImage : BannerImage
+  );
+  };
+
   return (
     <div className="home-container">
       <Navbar />
       <div className="home-banner-container">
-        <div className="home-image-section">
-          <img src={BannerImage} alt="" />
-        </div>
+        <motion.div
+            className="home-image-section"
+            onClick={handleClick}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+          <motion.img src={imageSrc} alt="" />
+    </motion.div>
         <p className="primary-text">
           <br></br>
           <br></br>
